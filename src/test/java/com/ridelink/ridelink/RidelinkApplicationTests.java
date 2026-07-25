a0481ac9059930cc9b9a716ad28b1191ecb60aad
dhavalbathe <dhavalbathe@gmail.com>
2026-07-25 15:47:01 +0530

    Initial Setup for RideLink Project Completed!

---
+++ src/test/java/com/ridelink/ridelink/RidelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridelink.ridelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
