e10b7ac3254468e656c6c26415d52087cdecb111
dhavalbathe <dhavalbathe@gmail.com>
2026-07-29 18:55:17 +0530

    feat: created exception classes to handle vehicle exceptions

---
+++ src/main/java/com/ridelink/ridelink/exception/vehicleException/VehicleAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.ridelink.ridelink.exception.vehicleException;
+
+public class VehicleAlreadyExistsException extends RuntimeException{
+    public VehicleAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/ridelink/ridelink/exception/vehicleException/VehicleNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ridelink.ridelink.exception.vehicleException;
+
+public class VehicleNotFoundException extends RuntimeException{
+    public VehicleNotFoundException(String message) {
+        super(message);
+    }
+}
