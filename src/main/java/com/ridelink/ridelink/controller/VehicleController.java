a858d4606fb6a389c2ecb6f50bd100863e887695
dhavalbathe <dhavalbathe@gmail.com>
2026-07-30 16:18:04 +0530

    modified: response body customized according to Api respnose class

--- src/main/java/com/ridelink/ridelink/controller/VehicleController.java
+++ src/main/java/com/ridelink/ridelink/controller/VehicleController.java
@@ -3,6 +3,7 @@ package com.ridelink.ridelink.controller;
 import com.ridelink.ridelink.dto.vehicleDto.CreateVehicleRequestDTO;
 import com.ridelink.ridelink.dto.vehicleDto.UpdateVehicleRequestDTO;
 import com.ridelink.ridelink.dto.vehicleDto.VehicleResponseDTO;
+import com.ridelink.ridelink.response.ApiResponse;
 import com.ridelink.ridelink.service.vehicleService.VehicleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,50 +21,52 @@ public class VehicleController {
     private final VehicleService vehicleService;
 
     @PostMapping
-    public ResponseEntity<VehicleResponseDTO> addVehicle(
+    public ResponseEntity<ApiResponse<VehicleResponseDTO>> addVehicle(
             @RequestBody @Valid CreateVehicleRequestDTO request) {
 
         VehicleResponseDTO response = vehicleService.addVehicle(request);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(response);
+                .body(ApiResponse.success("The New Vehicle Added Successfully.", response));
     }
 
     @GetMapping
-    public ResponseEntity<List<VehicleResponseDTO>> getMyVehicles() {
+    public ResponseEntity<ApiResponse<List<VehicleResponseDTO>>> getMyVehicles() {
 
         List<VehicleResponseDTO> response = vehicleService.getMyVehicles();
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success("All the Vehicle are fetched successfully.", response));
     }
 
     @GetMapping("/{vehicleId}")
-    public ResponseEntity<VehicleResponseDTO> getVehicleById(
+    public ResponseEntity<ApiResponse<VehicleResponseDTO>> getVehicleById(
             @PathVariable Long vehicleId) {
 
         VehicleResponseDTO response = vehicleService.getVehicleById(vehicleId);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(ApiResponse.success("Vehicle Fetched Successfully.", response));
     }
 
     @PutMapping("/{vehicleId}")
-    public ResponseEntity<VehicleResponseDTO> updateVehicle(
+    public ResponseEntity<ApiResponse<VehicleResponseDTO>> updateVehicle(
             @PathVariable Long vehicleId,
             @RequestBody @Valid UpdateVehicleRequestDTO request) {
 
         VehicleResponseDTO response =
                 vehicleService.updateVehicle(vehicleId, request);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(ApiResponse.success("Vehicle is updated successfully.", response));
     }
 
     @DeleteMapping("/{vehicleId}")
-    public ResponseEntity<Void> deleteVehicle(
+    public ResponseEntity<ApiResponse<Void>> deleteVehicle(
             @PathVariable Long vehicleId) {
 
         vehicleService.deleteVehicle(vehicleId);
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("The Vehicle is deleted successfully."));
     }
 }
